f0cadbc4218700206ec9cb7301a0d398688bcc16
hendersonanalytics <everett@hendersonanalytics.com>
2019-06-03 11:41:30 -0500

    init commit

--- dist/js/code-gov-web-components.js
+++ dist/js/code-gov-web-components.js
@@ -356,6 +356,77 @@ function (_HTMLElement) {
 
 
 customElements.define('filter-tag', FilterTag);
+'use strict';
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+(function () {
+  /*global HTMLElement*/
+  var GovBanner =
+  /*#__PURE__*/
+  function (_HTMLElement) {
+    _inherits(GovBanner, _HTMLElement);
+
+    function GovBanner() {
+      _classCallCheck(this, GovBanner);
+
+      // establish prototype chain
+      return _possibleConstructorReturn(this, _getPrototypeOf(GovBanner).call(this));
+    } // static get observedAttributes() {
+    //   return ['image', 'title'];
+    // }
+    // fires after the element has been attached to the DOM
+
+
+    _createClass(GovBanner, [{
+      key: "connectedCallback",
+      value: function connectedCallback() {
+        this.update();
+      } // attributeChangedCallback(attrName, oldVal, newVal) {
+      //   if (attrName === 'options') {
+      //     this.update();
+      //   }
+      // }
+
+    }, {
+      key: "update",
+      value: function update() {
+        this.innerHTML = "\n      <div class=\"usa-banner\">\n        <div class=\"usa-accordion\">\n            <header class=\"usa-banner__header\">\n                <div class=\"usa-banner__inner\">\n                    <div class=\"grid-col-auto\">\n                        <img class=\"usa-banner__header-flag\" src=\"../../dist/img/us_flag_small.png\" alt=\"U.S. flag\">\n                    </div>\n                    <div class=\"grid-col-fill tablet:grid-col-auto\">\n                        <p class=\"usa-banner__header-text\">An official website of the United States government</p>\n                        <p class=\"usa-banner__header-action\" aria-hidden=\"true\">Here\u2019s how you know</p>\n                    </div>\n                    <button class=\"usa-accordion__button usa-banner__button\" aria-expanded=\"false\" aria-controls=\"gov-banner\">\n              <span class=\"usa-banner__button-text\">Here\u2019s how you know</span>\n            </button>\n                </div>\n            </header>\n            <div class=\"usa-banner__content usa-accordion__content\" id=\"gov-banner\">\n                <div class=\"grid-row grid-gap-lg\">\n                    <div class=\"usa-banner__guidance tablet:grid-col-6\">\n                        <img class=\"usa-banner__icon usa-media-block__img\" src=\"../../dist/img/icon-dot-gov.svg\" alt=\"Dot gov\">\n                        <div class=\"usa-media-block__body\">\n                            <p>\n                                <strong>The .gov means it\u2019s official.</strong>\n                                <br> Federal government websites often end in .gov or .mil. Before sharing sensitive information, make sure you\u2019re on a federal government site.\n                            </p>\n                        </div>\n                    </div>\n                    <div class=\"usa-banner__guidance tablet:grid-col-6\">\n                        <img class=\"usa-banner__icon usa-media-block__img\" src=\"../../dist/img/icon-https.svg\" alt=\"Https\">\n                        <div class=\"usa-media-block__body\">\n                            <p>\n                                <strong>The site is secure.</strong>\n                                <br> The <strong>https://</strong> ensures that you are connecting to the official website and that any information you provide is encrypted and transmitted securely.\n                            </p>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        </div>\n      </div>\n      ";
+      }
+    }]);
+
+    return GovBanner;
+  }(_wrapNativeSuper(HTMLElement)); // let the browser know about the custom element
+
+  /*global customElements*/
+
+
+  customElements.define('gov-banner', GovBanner);
+})();
 "use strict";
 'use strict';
 
